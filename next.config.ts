479a2b6a0b708a23103e67a6bcca9d44998cfbd3
Lory-Stan TANASI <stantanasi@gmail.com>
2026-05-05 18:20:06 -0400

    feat(next): configure GitHub pages subdomain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/restaurant-website', 
+  assetPrefix: '/restaurant-website',
   images: {
     unoptimized: true,
   },
